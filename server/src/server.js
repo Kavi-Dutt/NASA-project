5f5f5a147cae0f0a2f41cf6b85a13571534b256f
Kavi Dutt Machkoli <kaviduttmachkoli@192.168.1.2>
2023-08-06 16:38:37 +0530

    connected to mongo server added planest and launch schema

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,23 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
+const MONGO_URL = 'mongodb+srv://nasa-api:liZJhmnn2ZjlFF6U@cluster1.mxsaicj.mongodb.net/nasa?retryWrites=true&w=majority'
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', ()=>{
+    console.log('MongoDB connection ready');
+})
+
+mongoose.connection.on('error', (err)=>{
+    console.error(err)
+})
 async function startServer() {
     try {
+        await mongoose.connect(MONGO_URL);
         await loadPlanetsData();
         server.listen(PORT, () => {
             console.log(`listening on port ${PORT}`);
